6c73c5410e6faf3342e42f76203249a76893a81f
Aakash <aaakash@academic.rrc.ca>
2025-04-03 04:03:04 -0500

    feat: configure Firebase admin SDK with hybrid local and env-based credential loading.

---
+++ src/config/firebase.ts
@@ -0,0 +1,14 @@
+import admin from "firebase-admin";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY
+  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
+  : require("../../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+});
+
+export default admin;
