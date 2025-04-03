5931ff51b009954071830200a4e33a8607a48f8c
Aakash <aaakash@academic.rrc.ca>
2025-04-03 04:42:06 -0500

    feat(auth): add /register route and connect to controller.

---
+++ src/api/v1/routes/auth.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { registerUser } from "../controllers/authController";
+
+const router = Router();
+router.post("/register", registerUser);
+export default router;
