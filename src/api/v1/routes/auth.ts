051df2885f9807fb72f2183dbee48afa708be747
Aakash <aaakash@academic.rrc.ca>
2025-04-03 05:51:02 -0500

    feat: add /auth/login route to handle user authentication.

--- src/api/v1/routes/auth.ts
+++ src/api/v1/routes/auth.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/authController";
+import { loginUser } from "../controllers/authController";
 
 const router = Router();
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+
 export default router;
