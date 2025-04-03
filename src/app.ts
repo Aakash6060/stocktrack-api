061a2b9ed55d7d0e6e79ac5ee4a9a6b886ba31a4
Aakash <aaakash@academic.rrc.ca>
2025-04-03 05:36:19 -0500

    chore: mount auth routes under /api/v1/auth path.

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { Request, Response } from "express";
+import authRoutes from "./api/v1/routes/auth";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+
+app.use("/api/v1/auth", authRoutes);
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to StockTrack API");
